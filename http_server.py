9c0686a3efac0681ac3bf1433a045f4a64bee1cd
Matt Briggs <mabrigg@microsoft.com>
2018-04-23 15:37:26 -0700

    adding initial work

--- http_server.py
+++ http_server.py
@@ -1,6 +1,7 @@
 import socket
 import sys
 import os
+import mimetypes
 
 def response_ok(body=b"This is a minimal response", mimetype=b"text/plain"):
     """
@@ -19,15 +20,17 @@ def response_ok(body=b"This is a minimal response", mimetype=b"text/plain"):
     """
 
     return b"\r\n".join([
-                b"HTTP/1.1 200 OK",
-                b"Content-Type: " + mimetype,
-                b"",
-                body,
+                b"HTTP/1.1 200 OK\r\n",
+                b"Content-Type: " + mimetype + b"\r\n",
+                b"\r\n",
+                body + b"\r\n",
             ])
 
 def parse_request(request):
     
     method, uri, version = request.split("\r\n")[0].split(" ")
+
+    print(method, uri, version)
 
     if method != "GET":
         raise NotImplementedError
@@ -51,7 +54,11 @@ def response_not_found():
     # You can re-use most of the code from the 405 Method Not
     # Allowed response.
 
-    pass
+    return b"\r\n".join([
+            b"HTTP/1.1 404 Not Found",
+            b"",
+            b"The resource you looking for has moved or been removed.",
+        ])
     
 
 def resolve_uri(uri):
@@ -89,14 +96,58 @@ def resolve_uri(uri):
     # Hint: When opening a file, use open(filename, "rb") to open and read the
     # file as a stream of bytes.
 
-    content = b"not implemented"
-    mime_type = b"not implemented"
+    def get_file(loc):
+        in_file = open(uri, "rb")
+        body = in_file.read()
+        in_file.close()
 
-    return content, mime_type
+        return body
 
+    print(type(uri))
+
+    uri = "webroot\\" + uri
+
+    if os.path.isdir(uri):
+        files = os.listdir(uri)
+        list_files = ""
+        for i in files:
+            list_files += i + "<br>"
+        body = str.encode(str(list_files))
+        mimetype = b"text/html"
+        return (body, mimetype)
+
+    elif os.path.isfile(uri):
+        print("is is a file")
+        ext = uri.split(".")[1]
+
+        if ext in ["text", "txt", "html", "htm"]:
+            body = get_file(uri)
+            mimetype = b"text/html"
+
+
+        elif ext in ["jpg", "jpeg"]:
+            body = get_file(uri)
+            mimetype = b"image/jpg"
+
+        elif ext == "png":
+            body = get_file(uri)
+            mimetype = b"image/png"
+
+        elif ext == "gif":
+            body = get_file(uri)
+            mimetype = b"image/gif"
+
+        else:
+            body = b'File type not supported.'
+            mimetype = b"text/html"
+
+        return (body, mimetype)
+
+    else:
+        response_not_found()
 
 def server(log_buffer=sys.stderr):
-    address = ('0.0.0.0', int(os.environ.get('PORT', 10000)))
+    address = ('localhost', int(os.environ.get('PORT', 10000)))
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     print("making a server on {0}:{1}".format(*address), file=log_buffer)
@@ -121,15 +172,19 @@ def server(log_buffer=sys.stderr):
 
                 try:
                     uri = parse_request(request)
+                    body, mimetype = resolve_uri(uri)
+                    print("URI: {}\nMimeType: {}\nBody: {}\n--30--\n".format(uri, mimetype, body))
+                    response = response_ok(body=body, mimetype=mimetype)
                 except NotImplementedError:
                     response = response_method_not_allowed()
-                else:
+                except NameError:
                     # TODO: resolve_uri will raise a NameError if the file
                     # specified by uri can't be found. If it does raise a
                     # NameError, then let response get response_not_found()
+                    response = response_not_found()
+                else:
                     # instead of response_ok()
-                    body, mimetype = resolve_uri(uri)
-                    response = response_ok(body=body, mimetype=mimetype)
+                    response_method_not_allowed()
 
                 conn.sendall(response)
             finally:
